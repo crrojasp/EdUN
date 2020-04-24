943e0b5d14f2a0338b658c9c88141303e9fe8666
crrojasp <crrojasp@unal.edu.co>
2020-04-24 17:48:38 -0500

    Proyect DataBases
    Ya están las soluciones e implementaciones de conexión

---
+++ app/src/main/java/com/agatone/edun/Entrega_Trabajo_Activity.java
@@ -0,0 +1,14 @@
+package com.agatone.edun;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Entrega_Trabajo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_entrega__trabajo_ );
+    }
+}
---
+++ app/src/main/java/com/agatone/edun/Estado_Entrega_Trabajo.java
@@ -0,0 +1,14 @@
+package com.agatone.edun;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Estado_Entrega_Trabajo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_estado__entrega__trabajo );
+    }
+}
