47a358679c7bfbdb8f58f4ff6735ba1fd110dc24
crrojasp <crrojasp@unal.edu.co>
2020-04-29 16:44:40 -0500

    proyect semifucionado
    Signed-off-by: crrojasp <crrojasp@unal.edu.co>

---
+++ app/src/main/java/com/agatone/edun/estructuras/Lista.java
@@ -0,0 +1,161 @@
+package com.agatone.edun.estructuras;
+
+
+import android.content.Context;
+
+import com.agatone.edun.Clases.Coneccion;
+import com.agatone.edun.Clases.archivo;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author cristian
+ */
+
+
+
+public class Lista  implements Response.Listener<JSONObject>,Response.ErrorListener{
+
+    private Nodo head;
+    private Nodo back;
+    public Context context;
+
+    public Nodo llenarLista(){
+        RequestQueue request;
+        JsonObjectRequest jeison;
+
+        request= Volley.newRequestQueue(context);
+        String url=null;
+        url="http://"+ Coneccion.host+"/listarArchivos.php";
+        jeison=new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        request.add(jeison);
+        return head;
+    }
+
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        archivo arc=null;
+        JSONArray json=response.optJSONArray("usuario");
+
+
+        try {
+            for(int i=0;i<json.length();i++){
+                int id;
+                String nombre,autor,dueno,tipo;
+                JSONObject jsonObject=json.getJSONObject(i);
+                id=jsonObject.optInt("id");
+                nombre=jsonObject.optString("nombreArchivo");
+                autor=jsonObject.optString("autorArchivo");
+                dueno=jsonObject.optString("duenoArchivo");
+                tipo=jsonObject.optString("tipo");
+
+                arc=new archivo(id,nombre,autor,dueno,tipo);
+                insert(arc);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public Lista(Context context){
+        this.context=context;
+        head=null;
+    };
+
+
+    public boolean insert(archivo arc){
+        boolean insertado=false;
+        Nodo node=new Nodo(arc);
+        Nodo previo,actual;
+
+
+        actual=head;
+        previo=null;
+        node=head.getNext();
+        head.setNext(node);
+        if(head.getNext()== node)
+            insertado = true;
+
+        return insertado;
+    }
+
+
+    public Nodo OrdenarNombre(){
+        Nodo ordenador=head;
+        Nodo antes=null,despues=null;
+        while(ordenador!=null){
+            if(ordenador.getArc().getNombre().
+                    compareToIgnoreCase(ordenador.getNext().getArc().getNombre())<=0){
+
+                if(antes==null){
+
+                }
+
+
+            }else{
+                antes=ordenador;
+                ordenador=ordenador.getNext();
+            }
+
+
+
+        }
+
+
+
+        return head;
+    }
+
+
+    public boolean delete(int id){
+        boolean del=false;
+        Nodo prv,ptr=head;
+        while(ptr!=null & ptr.getArc().getId()<id){
+            prv=ptr;
+            ptr=ptr.getNext();
+        }
+        if(ptr!=null&ptr.getArc().getId()==id){
+            del=true;
+            prv=ptr.getNext();
+        }
+        return del;
+    }
+
+
+
+    public Nodo buscarId(int id){
+        Nodo nodo=head;
+        while(nodo!=null){
+            if(nodo.getArc().getId()==id){
+                return nodo;
+            }
+            nodo=nodo.getNext();
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/agatone/edun/estructuras/Nodo.java
@@ -0,0 +1,44 @@
+package com.agatone.edun.estructuras;
+
+import com.agatone.edun.Clases.archivo;
+
+/**
+ *
+ * @author cristian
+ */
+public class Nodo {
+    private archivo arc;
+
+    private Nodo next;
+    private Nodo before;
+
+    public  Nodo(archivo arc){
+        this.arc=arc;
+        this.next=null;
+        this.before=null;
+    }
+
+    public archivo getArc(){
+        return arc;
+    }
+
+    public void setArc(archivo arc) {
+        this.arc = arc;
+    }
+
+    public Nodo getBefore() {
+        return before;
+    }
+
+    public void setBefore(Nodo before) {
+        this.before = before;
+    }
+
+    public Nodo getNext(){
+        return next;
+    }
+
+    public void setNext(Nodo sig){
+        this.next=sig;
+    }
+}
