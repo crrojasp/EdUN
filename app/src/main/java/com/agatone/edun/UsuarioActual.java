0f42289f94345973d6972fd60805d2c949a18d58
cristian_2024 <crramirezma@unal.edu.co>
2020-05-12 16:05:40 -0500

    Clase UsuarioActual
    Esta clase esta planeada para controlar el usuario con el que se ingreso a la app

---
+++ app/src/main/java/com/agatone/edun/UsuarioActual.java
@@ -0,0 +1,14 @@
+package com.agatone.edun;
+
+import com.agatone.edun.Clases.Usuario;
+
+
+/*
+*Con esta clase se planea poder compartir informacion del usuario que inicio sesio en la app
+* ya que se trata de un usuario estatico, este pertenece a la clase, y por ello no es necesario hacer una instancia de el
+*
+ */
+public class UsuarioActual {
+    static Usuario usuario;
+
+}
