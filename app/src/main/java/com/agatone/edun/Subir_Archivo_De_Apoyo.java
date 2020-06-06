a8dd68dfb9d2df8d2f5502723823d92185958372
crrojasp <crrojasp@unal.edu.co>
2020-06-06 11:45:03 -0500

    Intentaré hacer lo de las fechas en arboles, ya aquí esta lo ultimo que usted ha hecho con lo de las fechas y el principal que habia hecho yo con anterioridad

---
+++ app/src/main/java/com/agatone/edun/Subir_Archivo_De_Apoyo.java
@@ -0,0 +1,16 @@
+package com.agatone.edun;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.agatone.edun.R;
+
+public class Subir_Archivo_De_Apoyo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_subir_archivos_de_apoyo );
+    }
+}
