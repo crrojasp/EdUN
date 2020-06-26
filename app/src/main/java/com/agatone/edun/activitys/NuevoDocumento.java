0a3d6cf2fb3c28643be4cad0e494c2dfaa48fc24
cristian2024 <61856006+cristian2024@users.noreply.github.com>
2020-06-26 13:05:33 -0500

    Nose que cambie

---
+++ app/src/main/java/com/agatone/edun/activitys/NuevoDocumento.java
@@ -0,0 +1,23 @@
+package com.agatone.edun.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.agatone.edun.R;
+
+public class NuevoDocumento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_documento);
+
+
+    }
+
+    public void regresar(View view) {
+
+    }
+}
