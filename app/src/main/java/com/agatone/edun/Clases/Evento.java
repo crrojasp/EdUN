4ba0465b4d9e42c5171e4e712422b98e31794ac3
crrojasp <crrojasp@unal.edu.co>
2020-07-04 12:43:57 -0500

    Creación clase

---
+++ app/src/main/java/com/agatone/edun/Clases/Evento.java
@@ -0,0 +1,13 @@
+package com.agatone.edun.Clases;
+
+import java.util.Date;
+
+public class Evento {
+    private Date Año;
+    private Date Mes;
+    private Date Dia;
+
+    public Evento(){
+
+    }
+}
