3d46f253ac9640036bcadea8ef2d7ee3f1cc4468
cristian2024 <61856006+cristian2024@users.noreply.github.com>
2020-07-04 18:49:07 -0500

    constructores, getter y setter

--- app/src/main/java/com/agatone/edun/Clases/Evento.java
+++ app/src/main/java/com/agatone/edun/Clases/Evento.java
@@ -6,15 +6,78 @@ public class Evento {
 
     private int dia, mes, ano, hora, minuto, id;
 
-    public Evento(){
-
+    public Evento(String eventoNb, String caracteristica, int dia, int mes, int ano, int hora, int minuto, int id) {
+        this.EventoNb = eventoNb;
+        this.caracteristica = caracteristica;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.hora = hora;
+        this.minuto = minuto;
+        this.id = id;
     }
 
-
-    public String getNombre ( int id ) {
-        for (int h = 0; h != id; h++) {
-            throw new IndexOutOfBoundsException ();
-        }
+    public String getEventoNb() {
         return EventoNb;
+    }
+
+    public void setEventoNb(String eventoNb) {
+        EventoNb = eventoNb;
+    }
+
+    public String getCaracteristica() {
+        return caracteristica;
+    }
+
+    public void setCaracteristica(String caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
